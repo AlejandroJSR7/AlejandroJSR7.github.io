371a0708f55cb35622d260ecd21a7b5a480107e6
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-02-28 01:43:27 -0500

    Update Design to My Portfolio Page

--- scripts/components/scroll-menu.js
+++ scripts/components/scroll-menu.js
@@ -6,7 +6,7 @@ $(function(){
              $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
              if ($target.length) {
                  var targetOffset = $target.offset().top;
-                 $('html,body').animate({scrollTop: targetOffset}, 1800);
+                 $('html,body').animate({scrollTop: targetOffset}, 500);
                  return false;
             }
        }
--- scripts/components/scroll-up.js
+++ scripts/components/scroll-up.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	
 	//Click event to scroll to top
 	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0}, 800);
+		$('html, body').animate({scrollTop : 0}, 500);
 		return false;
 	});
 	
