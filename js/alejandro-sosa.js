8b70edde65c6b5f453b6a19a48f93026043f7616
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-02-29 05:23:17 -0500

    Update portfolio, New Design & Projects

--- js/alejandro-sosa.js
+++ js/alejandro-sosa.js
@@ -1,1 +1,33 @@
-$("#demo01").animatedModal(),$(function(){$("a[href*=#]").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var o=$(this.hash);if(o=o.length&&o||$("[name="+this.hash.slice(1)+"]"),o.length){var t=o.offset().top;return $("html,body").animate({scrollTop:t},1800),!1}}})}),$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>100?$(".scrollToTop").fadeIn():$(".scrollToTop").fadeOut()}),$(".scrollToTop").click(function(){return $("html, body").animate({scrollTop:0},800),!1})});
+$(".js-openModalContact").animatedModal();
+$(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body').animate({scrollTop: targetOffset}, 500);
+          return false;
+        }
+      }
+   });
+});
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0}, 500);
+		return false;
+	});
+	
+});
