f33e1540f8af3514ba6825818bef4255ad6ded4e
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-07-10 12:02:44 -0500

    spotify, vanillajs modal, update skills

--- js/alejandro-sosa.js
+++ js/alejandro-sosa.js
@@ -1,4 +1,25 @@
-$(".js-openModalContact").animatedModal();
+$(document).ready(function(){
+
+  var modal = document.getElementById("modal-contact");
+  var btnOpen = document.getElementById("openModalContact");
+  var btnClose = document.getElementById("closeModalContact");
+
+  btnOpen.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  btnClose.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function () {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+});
+
 $(function(){
   $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
